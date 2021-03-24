343eb436893f248a2fb4044bfeb8812190dd94a4
Satish Prasad <spd61185@gmail.com>
2021-03-24 07:31:02 +0530

    LanguageFundamentals

--- Java_fundamentals_3/src/com/spd/entity/Rectangle.java
+++ Java_fundamentals_3/src/com/spd/entity/Rectangle.java
@@ -58,5 +58,30 @@ public class Rectangle {
 	public int getArea() {
 		return width*breadth;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + breadth;
+		result = prime * result + width;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rectangle other = (Rectangle) obj;
+		if (breadth != other.breadth)
+			return false;
+		if (width != other.width)
+			return false;
+		return true;
+	}
 	
 }
