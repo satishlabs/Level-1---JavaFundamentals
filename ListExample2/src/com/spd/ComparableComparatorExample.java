9f4b1b6d6a4c8ab68b6dbab92ca3667330fec623
Satish Prasad <spd61185@gmail.com>
2021-03-27 12:56:35 +0530

    CollectionFramework

---
+++ ListExample2/src/com/spd/ComparableComparatorExample.java
@@ -0,0 +1,45 @@
+package com.spd;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Example illustrating how to use Comparable 
+ * for sorting Employee entities
+ * @author Satish Prasad
+ *
+ */
+public class ComparableComparatorExample {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		List<Employee> employeeList = new ArrayList<Employee>();
+		addEmployee(employeeList);
+		Collections.sort(employeeList);
+		printEmployee(employeeList);
+	}
+
+	/**Method to traverse through the employee Collection
+	 * @param employeeList
+	 */
+	private static void printEmployee(List<Employee> employeeList) {
+		for(Employee emp: employeeList) {
+			System.out.println(emp);
+		}
+		
+	}
+
+	/**
+	 * Method to add employee's to collection
+	 * @param employeeList
+	 */
+	private static void addEmployee(List<Employee> employeeList) {
+		employeeList.add(new Employee(100, "Satish", 50000.00));
+		employeeList.add(new Employee(300, "Dhoni", 120000.40));
+		employeeList.add(new Employee(400, "Virat", 150000.02));
+		employeeList.add(new Employee(200, "Rohit", 80000.34));
+		
+	}
+}
